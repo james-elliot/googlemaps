a2d17550e11d223f7c0c99b62f80baacc95d684d
Jean-Marc Alliot <jm@alliot.org>
2022-11-02 00:35:53 +0100

    Prelim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use serde_json::{Result, Value};
+use std::fs;
+
+fn test<'a>(v:&'a Value,i:usize) -> Result<(i64,i64,&'a str)> {
+    let t: &Value = &v["locations"][i];
+    let time = 	t["timestamp"].as_str().unwrap();
+    let lat = 	t["latitudeE7"].as_i64().unwrap();
+    let lon = 	t["longitudeE7"].as_i64().unwrap();
+    Ok((lat,lon,time))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("Records.json").expect("Something went wrong reading the file");
+    let v: Value = serde_json::from_str(&contents).unwrap();
+    println!("{}",v["locations"][0]);
+//    let t:Value = v["locations"][0].clone();
+    let t: &Value = &v["locations"][0];
+    let time = 	t["timestamp"].as_str().unwrap();
+    let lat = 	t["latitudeE7"].as_i64().unwrap();
+    let lon = 	t["longitudeE7"].as_i64().unwrap();
+    println!(
+	"{} {} {}",
+	time,
+	lat,
+	lon
+    );
 }
